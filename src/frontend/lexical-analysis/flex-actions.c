4b7de92f7014daa7ded5420b339dd92b8ecd5cf2
Reversive <matiaspavan@gmail.com>
2023-09-23 13:02:42 -0300

    feat(flex): action definition for logical and numeric operators

--- src/frontend/lexical-analysis/flex-actions.c
+++ src/frontend/lexical-analysis/flex-actions.c
@@ -19,79 +19,151 @@
  * (mediante $1, $2, $3, etc.).
  */
 
-char * copyLexeme(const char * lexeme, const int length) {
-	char * lexemeCopy = (char *) calloc(length + 1, sizeof(char));
+char *copyLexeme(const char *lexeme, const int length)
+{
+	char *lexemeCopy = (char *)calloc(length + 1, sizeof(char));
 	strncpy(lexemeCopy, lexeme, length);
 	return lexemeCopy;
 }
 
-void BeginCommentPatternAction() {
+void BeginCommentPatternAction()
+{
 	LogDebug("[Flex] [COMMENT] BeginCommentPatternAction............................");
 }
 
-void EndCommentPatternAction() {
+void EndCommentPatternAction()
+{
 	LogDebug("[Flex] [COMMENT] EndCommentPatternAction..............................");
 }
 
-token AdditionOperatorPatternAction() {
+token AdditionOperatorPatternAction()
+{
 	LogDebug("[Flex] AdditionOperatorPatternAction: '+'.");
 	yylval.token = ADD;
 	return ADD;
 }
 
-token CloseParenthesisPatternAction() {
+token CloseParenthesisPatternAction()
+{
 	LogDebug("[Flex] CloseParenthesisPatternAction: ')'.");
 	yylval.token = CLOSE_PARENTHESIS;
 	return CLOSE_PARENTHESIS;
 }
 
-token DivisionOperatorPatternAction() {
+token DivisionOperatorPatternAction()
+{
 	LogDebug("[Flex] DivisionOperatorPatternAction: '/'.");
 	yylval.token = DIV;
 	return DIV;
 }
 
-token IntegerPatternAction(const char * lexeme, const int length) {
+token IntegerPatternAction(const char *lexeme, const int length)
+{
 	LogDebug("[Flex] IntegerPatternAction: '%s' (length = %d).", lexeme, length);
-	char * lexemeCopy = copyLexeme(lexeme, length);
+	char *lexemeCopy = copyLexeme(lexeme, length);
 	yylval.integer = atoi(lexemeCopy);
 	free(lexemeCopy);
 	return INTEGER;
 }
 
-token MultiplicationOperatorPatternAction() {
+token MultiplicationOperatorPatternAction()
+{
 	LogDebug("[Flex] MultiplicationOperatorPatternAction: '*'.");
 	yylval.token = MUL;
 	return MUL;
 }
 
-token OpenParenthesisPatternAction() {
+token OpenParenthesisPatternAction()
+{
 	LogDebug("[Flex] OpenParenthesisPatternAction: '('.");
 	yylval.token = OPEN_PARENTHESIS;
 	return OPEN_PARENTHESIS;
 }
 
-token SubtractionOperatorPatternAction() {
+token SubtractionOperatorPatternAction()
+{
 	LogDebug("[Flex] SubtractionOperatorPatternAction: '-'.");
 	yylval.token = SUB;
 	return SUB;
 }
 
-token UnknownPatternAction(const char * lexeme, const int length) {
-	char * lexemeCopy = copyLexeme(lexeme, length);
+token UnknownPatternAction(const char *lexeme, const int length)
+{
+	char *lexemeCopy = copyLexeme(lexeme, length);
 	LogDebug("[Flex] UnknownPatternAction: '%s' (length = %d).", lexemeCopy, length);
 	free(lexemeCopy);
 	yylval.token = ERROR;
-	// Al emitir este token, el compilador aborta la ejecución.
 	return ERROR;
 }
 
-void IgnoredPatternAction(const char * lexeme, const int length) {
-	char * lexemeCopy = copyLexeme(lexeme, length);
+token EqualOperatorPatternAction()
+{
+	LogDebug("[Flex] EqualOperatorPatternAction: '='.");
+	yylval.token = EQUAL;
+	return EQUAL;
+}
+
+token NotEqualOperatorPatternAction()
+{
+	LogDebug("[Flex] NotEqualOperatorPatternAction: '!='.");
+	yylval.token = NOT_EQUAL;
+	return NOT_EQUAL;
+}
+
+token LessThanOperatorPatternAction()
+{
+	LogDebug("[Flex] LessThanOperatorPatternAction: '<'.");
+	yylval.token = LESS_THAN;
+	return LESS_THAN;
+}
+
+token LessThanOrEqualOperatorPatternAction()
+{
+	LogDebug("[Flex] LessThanOrEqualOperatorPatternAction: '<='.");
+	yylval.token = LESS_THAN_OR_EQUAL;
+	return LESS_THAN_OR_EQUAL;
+}
+
+token GreaterThanOperatorPatternAction()
+{
+	LogDebug("[Flex] GreaterThanOperatorPatternAction: '>'.");
+	yylval.token = GREATER_THAN;
+	return GREATER_THAN;
+}
+
+token GreaterThanOrEqualOperatorPatternAction()
+{
+	LogDebug("[Flex] GreaterThanOrEqualOperatorPatternAction: '>='.");
+	yylval.token = GREATER_THAN_OR_EQUAL;
+	return GREATER_THAN_OR_EQUAL;
+}
+
+token OrOperatorPatternAction()
+{
+	LogDebug("[Flex] OrOperatorPatternAction: '||'.");
+	yylval.token = OR;
+	return OR;
+}
+
+token AndOperatorPatternAction()
+{
+	LogDebug("[Flex] AndOperatorPatternAction: '&&'.");
+	yylval.token = AND;
+	return AND;
+}
+
+token NotOperatorPatternAction()
+{
+	LogDebug("[Flex] NotOperatorPatternAction: '!'.");
+	yylval.token = NOT;
+	return NOT;
+}
+
+void IgnoredPatternAction(const char *lexeme, const int length)
+{
+	char *lexemeCopy = copyLexeme(lexeme, length);
 	LogRaw("[DEBUG] [Flex] IgnoredPatternAction: '");
 	LogText(lexemeCopy, length);
 	LogRaw("' (length = %d).\n", length);
 	free(lexemeCopy);
-	// Como no debe hacer nada con el patrón, solo se loguea en consola.
-	// No se emite ningún token.
 }
